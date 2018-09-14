feb16f06d25105b822c98299ba7b88474c1b6a01
juliengrima <gogrinformatique@gmail.com>
2018-09-14 14:51:02 +0200

    kd

--- front/src/App.js
+++ front/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
               {
                   data.map(function(resto){
                     return <td>{resto.name}</td>;
+
                   })
               }
           </tr>
