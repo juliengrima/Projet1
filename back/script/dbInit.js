34bd1e9e44ee8c0aa79ac5030edfee98e0ec313f
Alexandre COIN <alexandre.pc.coin@gmail.com>
2018-09-17 14:34:03 +0200

    db schema update

--- back/script/dbInit.js
+++ back/script/dbInit.js
@@ -10,15 +10,17 @@ fs.readFile('../restaurants.json', (err, data) => {
         name: element.name,
         address1: element.address1,
         address2: element.address2,
+        latitude: element.latitude,
+        longitude: element.longitude,
         image: element.image_url,
         to_website: element.to_website,
         note: element.editorial_rating
       }
     })
-    .filter(element => element.name && element.address1 && element.address2 && element.image && element.to_website && element.note)
+    .filter(element => element.name && element.address1 && element.address2 && element.latitude && element.longitude && element.image && element.to_website && element.note)
     .forEach(element => {
-      const sqlQuery = "INSERT INTO list (name, address1, address2, image, to_website, note) VALUES (?,?,?,?,?,?)"
-      connection.query(sqlQuery, [element.name, element.address1, element.address2, element.image, element.to_website, element.note], function (error, results, fields) {
+      const sqlQuery = "INSERT INTO list (name, address1, address2, latitude, longitude, image, to_website, note) VALUES (?,?,?,?,?,?)"
+      connection.query(sqlQuery, [element.name, element.address1, element.address2, element.latitude, element.longitude, element.image, element.to_website, element.note], function (error, results, fields) {
         if (error) throw error
         console.log(results);
       })
