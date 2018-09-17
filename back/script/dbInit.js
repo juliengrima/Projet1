8f073550c7c6b06c28ea628b768f9adb8b88ee1b
Alexandre COIN <alexandre.pc.coin@gmail.com>
2018-09-17 14:44:27 +0200

    functional map

--- back/script/dbInit.js
+++ back/script/dbInit.js
@@ -19,7 +19,7 @@ fs.readFile('../restaurants.json', (err, data) => {
     })
     .filter(element => element.name && element.address1 && element.address2 && element.latitude && element.longitude && element.image && element.to_website && element.note)
     .forEach(element => {
-      const sqlQuery = "INSERT INTO list (name, address1, address2, latitude, longitude, image, to_website, note) VALUES (?,?,?,?,?,?)"
+      const sqlQuery = "INSERT INTO list (name, address1, address2, latitude, longitude, image, to_website, note) VALUES (?,?,?,?,?,?,?,?)"
       connection.query(sqlQuery, [element.name, element.address1, element.address2, element.latitude, element.longitude, element.image, element.to_website, element.note], function (error, results, fields) {
         if (error) throw error
         console.log(results);
