1aa3a205155a04e40475db070b1f6cc21825adb2
Alexandre Teyssier <alextey28@gmail.com>
2018-09-18 09:23:12 +0200

    Start style on home page

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar.component';
 import ListRestaurant from './components/list-restaurants.component';
-import data from './restaurants.json'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <ul>
-          {
-            data.map(function (resto) {
-              return <li>{resto.name}</li>;
-            })
-          }
-        </ul> */}
-
+        <Navbar />
         <ListRestaurant />
       </div>
     );
--- front/src/components/list-restaurants.component.js
+++ front/src/components/list-restaurants.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './list-restaurant.css';
+import './../styles/list-restaurants.css';
 class ListRestaurant extends Component {
   constructor(props) {
     super(props)
@@ -28,23 +28,41 @@ class ListRestaurant extends Component {
 
   renderRestaurantsList() {
     const restaurantsList = this.state.restaurants.map((restaurant, i) =>
-      <div key={i}>
-        <h1>
-          Nom: {restaurant.name}
-        </h1>
-        <img className="image" src={restaurant.image} />
-        <p>Adresse: {restaurant.address1} {restaurant.address2}</p>
-        <p className="note">Note: {restaurant.note}/10</p>
+      <div className="mx-auto restaurant mt-2" key={i}>
+        <h2 className="mt-4 mb-4">
+          {restaurant.name}
+        </h2>
+        <img className="image" src={restaurant.image} alt="restaurant" />
+        <h3>{restaurant.address1} {restaurant.address2}</h3>
+        <p className="note">{restaurant.note}/5</p>
         <a href={restaurant.to_website} target="_blank">Lien vers le site</a>
       </div>
     )
-    return <div>{restaurantsList}</div>
+    return <div className="row mt-2">{restaurantsList}</div>
   }
 
   render() {
     return (
       <div>
-        {this.renderRestaurantsList()}
+        <div className="container-fluid header">
+          <div className="header-brightness"></div>
+          <div className="row h-100">
+            <div className="mx-auto my-auto header-title">
+              <h1 className="mt-2">Vous voulez trouvez un restaurant à Paris ?</h1>
+              <h2 className="mt-2">Rechercher parmis plus de nos 400 restaurants</h2>
+              <button className="btn btn-primary btn-lg mt-2">Voir notre gallerie</button>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="mx-auto">
+              <h1>Restaurants</h1>
+              <hr />
+            </div>
+          </div>
+          {this.renderRestaurantsList()}
+        </div>
       </div>
     )
   }
