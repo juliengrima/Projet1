84be874c5c5e2c7776036d70eb222cfed999be7b
Alexandre COIN <alexandre.pc.coin@gmail.com>
2018-09-18 09:36:33 +0200

    css add

--- front/src/components/restaurantsMap.component.js
+++ front/src/components/restaurantsMap.component.js
@@ -16,8 +16,8 @@ const RestaurantsMap = withScriptjs(
       center={{ lat: 48.856614, lng: 2.3522219 }}
     >
       {markers}
-    </GoogleMap>);
-}
+    </GoogleMap>);   
+  }
 ))
 
 export default RestaurantsMap;
